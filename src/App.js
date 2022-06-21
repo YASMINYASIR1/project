120a06a0d87b1f07a5f9a3c8c95a1a1ec3c0dbd1
yasmin yasir <yasmin.yasir19@gmail.com>
2022-06-21 15:45:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from "react";
+import { Notifications } from 'react-push-notification';
+import addNotification from 'react-push-notification';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [name,setName] = useState("");
+
+function warningNotification (){
+	addNotification({
+	title: 'Warning',
+	subtitle: 'You are out of the school',
+	message: 'goodbye',
+	theme: 'red',
+	closeButton:"X",
+	})
+};
+
+function successNotification (){
+	addNotification({
+	title: 'Success',
+	subtitle: 'You have successfully submitted',
+	message: 'Welcome',
+	theme: 'light',
+	closeButton:"X",
+	backgroundTop:"green",
+	backgroundBottom:"yellowgreen",
+  // colorTop: 'green',
+  // colorBottom: 'darkgreen',
+  // closeButton: 'Go away',
+  // native: boolean,
+	})
+};
+
+function handleSubmit(e){
+	e.preventDefault();
+	if(name === "out")
+	warningNotification();
+	else
+	successNotification()
+}
+
+return (
+	<div className="App">
+	<Notifications />
+	<h1>Hey there!</h1>
+	<form >
+		<label>Name:</label>
+		<input name="name" value={name}
+		onChange={(e) => setName(e.target.value)}
+		/>
+		<button onClick={handleSubmit}
+		type="submit">Submit
+		</button>
+	</form>
+	</div>
+);
 }
 
 export default App;
